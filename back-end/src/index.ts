d99ebb0225adaa89c05203bbdc8d15cf9d68d69d
adnanbezerra <adnanbezerra@proton.me>
2023-07-26 16:35:11 -0300

    fix(back): :bug: connect the userRouter to the index

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,13 +2,14 @@ import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import errorHandler from './middlewares/ErrorHandler';
+import { UserRouter } from './routes/UserRouter';
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 
 // routes
-// server.use();
+server.use(UserRouter);
 server.use(errorHandler);
 
 export default server;
